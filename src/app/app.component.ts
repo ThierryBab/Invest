7e2a8cf4b729249cbd0793d8e7c802e54dc5bc49
ThierryBab <Thierry.babault.20@hotmail.fr>
2024-10-01 18:11:40 -0400

    nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { InvestHomeComponent } from './component/invest-home/invest-home.component';
 import { AboutComponent } from './component/about/about.component';
+import { AppBarModule, NavigationModule } from "@progress/kendo-angular-navigation";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InvestHomeComponent, AboutComponent],
+  imports: [RouterOutlet, RouterLink, InvestHomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './component/about/about.component';
+import { InvestHomeComponent } from './component/invest-home/invest-home.component';
+import { CarsComponent } from './component/cars/cars.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'invest',
+        component: InvestHomeComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'cars',
+        component: CarsComponent,
+      },
+];
